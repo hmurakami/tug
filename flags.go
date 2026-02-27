09829f671ff1efc2f558601128f6d88dde2fced2
Tetsuro Mikami <tetsuro@mickamy.com>
2026-02-27 14:56:54 +0900

    fix: return joined values from stringSlice.String()

--- flags.go
+++ flags.go
@@ -1,9 +1,11 @@
 package main
 
+import "strings"
+
 // stringSlice implements flag.Value for repeatable string flags.
 type stringSlice []string
 
-func (s *stringSlice) String() string { return "" }
+func (s *stringSlice) String() string { return strings.Join(*s, ",") }
 func (s *stringSlice) Set(v string) error {
 	*s = append(*s, v)
 	return nil
